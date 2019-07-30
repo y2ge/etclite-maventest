290669700d2cd4ef5420f8eda4ad92c5b2830fc1
Y2GE <Y2GE@D234Q22.lbd.local>
2019-07-30 12:44:29 -0400

    Creating Project in Github repo

---
+++ etclite.maventest/src/test/java/org/etclite/maventest/etclite_maventest.java
@@ -0,0 +1,35 @@
+package org.etclite.maventest;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class etclite_maventest {
+  @Test
+  public void sum() {
+	  System.out.println("Sum Test");
+	  int a = 10;
+	  int b = 20;
+	  Assert.assertEquals(30, a+b);
+	  }
+  @Test
+  public void sub() {
+	  System.out.println("sub Test");
+	  int a = 10;
+	  int b = 20;
+	  Assert.assertEquals(10, b-a);
+	  }
+  @Test
+  public void div() {
+	  System.out.println("Div Test");
+	  int a = 10;
+	  int b = 20;
+	  Assert.assertEquals(2, b/a);
+	  }
+  @Test
+  public void mul() {
+	  System.out.println("Mul Test");
+	  int a = 10;
+	  int b = 20;
+	  Assert.assertEquals(200, a*b);
+	  }
+}
